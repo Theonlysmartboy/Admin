f74283a8860a3fd24261b4452f8953cad3fdb134
TheOnlySmartBoy <o2jose43@gmail.com>
2019-08-30 19:30:11 +0300

    Modified the users migration and added the restaurant migration

--- database/migrations/2019_08_30_120756_create_categories_table.php
+++ database/migrations/2019_08_30_120756_create_categories_table.php
@@ -4,17 +4,18 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCategoriesTable extends Migration
-{
+class CreateCategoriesTable extends Migration {
+
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
@@ -24,8 +25,8 @@ class CreateCategoriesTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('categories');
     }
+
 }
